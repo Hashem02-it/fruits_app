77e7913e8fdce373fd90509538b3aee0f652588b
Hashem02-it <hashemabuain02it@gmail.com>
2024-09-09 00:58:39 +0300

    spalash view logic done

--- lib/core/helper_functions/on_generate_routes.dart
+++ lib/core/helper_functions/on_generate_routes.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fruits_app/features/splash/presentation/views/on_boarding/presentation/views/on_boarding.dart';
 import 'package:fruits_app/features/splash/presentation/views/splash_view.dart';
 
 Route<dynamic> onGenerateRoute(RouteSettings settings) {
@@ -6,6 +7,8 @@ Route<dynamic> onGenerateRoute(RouteSettings settings) {
     case SplashView.routeName:
       return MaterialPageRoute(builder: (context) => const SplashView());
 
+    case OnBoardingView.routeName:
+      return MaterialPageRoute(builder: (context) => const OnBoardingView());
     default:
       return MaterialPageRoute(builder: (context) => const Scaffold());
   }
--- lib/widgets/splashview_body.dart
+++ lib/widgets/splashview_body.dart
@@ -1,14 +1,49 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:fruits_app/core/utils/app_images.dart';
+import 'package:fruits_app/features/splash/presentation/views/on_boarding/presentation/views/on_boarding.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody> {
+  @override
+  void initState() {
+    excuteNavigation();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Column(
-      children: [SvgPicture.asset(Assets.assetsImagesPlant)],
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            SvgPicture.asset(Assets.assetsImagesPlant),
+          ],
+        ),
+        SvgPicture.asset(Assets.assetsImagesLogo),
+        SvgPicture.asset(
+          Assets.assetsImagesSplashBottom,
+          fit: BoxFit.fill,
+        )
+      ],
+    );
+  }
+
+  void excuteNavigation() {
+    Future.delayed(
+      Duration(seconds: 3),
+      () {
+        Navigator.pushReplacementNamed(context, OnBoardingView.routeName);
+      },
     );
   }
 }
