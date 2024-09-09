038a994e5a04bd5283940f93f1c21861b5006ccb
Hashem02-it <hashemabuain02it@gmail.com>
2024-09-09 20:31:26 +0300

    ui done

--- lib/features/splash/presentation/views/widgets/onboardingview_body.dart
+++ lib/features/splash/presentation/views/widgets/onboardingview_body.dart
@@ -1,5 +1,9 @@
+import 'package:dots_indicator/dots_indicator.dart';
 import 'package:flutter/material.dart';
+import 'package:fruits_app/constants.dart';
+import 'package:fruits_app/core/utils/app_colors.dart';
 import 'package:fruits_app/features/splash/presentation/views/widgets/onboarding_pageview.dart';
+import 'package:fruits_app/widgets/custom_button.dart';
 
 class OnBoardingViewBody extends StatelessWidget {
   const OnBoardingViewBody({super.key});
@@ -9,6 +13,23 @@ class OnBoardingViewBody extends StatelessWidget {
     return Column(
       children: [
         Expanded(child: OnboardingPageview()),
+        DotsIndicator(
+          dotsCount: 2,
+          decorator: DotsDecorator(
+            activeColor: AppColors.primaryColor,
+            color: AppColors.primaryColor.withOpacity(.5),
+          ),
+        ),
+        SizedBox(
+          height: 29,
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: kHorizantalPadding),
+          child: CustomButton(onPressed: () {}, text: 'ابدأ الان'),
+        ),
+        SizedBox(
+          height: 43,
+        ),
       ],
     );
   }
